287f23070969ba6f9a864cc1efe10f8846a6c437
Erik Wahlberger <ewahlberger@gmail.com>
2021-11-23 19:18:14 +0100

    Add comments to pricing types

--- pricing/types/types.go
+++ pricing/types/types.go
@@ -1,9 +1,14 @@
+// Package containing data types for cloud provider pricing information
 package types
 
 import ()
 
+// Author: Erik Wahlberger
+// Type to represent the unit of measure for prices
 type Unit uint
 
+// Author: Erik Wahlberger
+// Enum for Unit type, containing the possible values to choose from (enum)
 const (
 	Unknown Unit = iota
 	OneHour
@@ -11,6 +16,8 @@ const (
 	OneSecond
 )
 
+// Author: Erik Wahlberger
+// Returns a string representation for the Unit value, or an empty string if an invalid value was provided
 func (u Unit) String() string {
 	switch u {
 	case OneHour:
@@ -24,6 +31,7 @@ func (u Unit) String() string {
 	return ""
 }
 
+// Author: Erik Wahlberger
 type CloudProviderPrice struct {
 	Price float64
 	Unit  Unit
