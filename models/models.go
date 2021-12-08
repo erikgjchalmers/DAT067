076bbbc485c5d48be905d9303f00d468c9f495a0
erikgj <erikgj@student.chalmers.se>
2021-12-08 11:05:42 +0100

    added function and test for normalizing slices

--- models/models.go
+++ models/models.go
@@ -1,17 +1,19 @@
 package models
 
 // [price/hour] * hour
-type IcostCalculator interface {
-	CalculateCost(allocation, usage []float64,
-		nodePrice, hours float64) float64
+type ICostCalculator interface {
+	CalculateCost(
+		allocation []float64,
+		usage [][]float64,
+		nodePrice, hours float64) []float64
 }
 
 //Badmodel
 type BadModel struct {
 }
 
-func (m BadModel) CalculateCost(capacity []float64, usage []float64, nodePrice float64, hours float64) float64 {
-	return nodePrice * hours * (usage[0] * usage[1]) / (capacity[0] * capacity[1])
+func (m BadModel) CalculateCost(capacity []float64, usage []float64, nodePrice float64, hours float64) []float64 {
+	return []float64{nodePrice * hours * (usage[0] * usage[1]) / (capacity[0] * capacity[1])}
 }
 
 //Goodmodel
@@ -19,6 +21,36 @@ type GoodModel struct {
 	balance []float64
 }
 
-func (m GoodModel) CalculateCost(max [:]float64, usage [:]float64, nodePrice float64, hours float64) float64 {
-	return 5.0
+func (m GoodModel) CalculateCost(nodeResources []float64, usagePerContainer [][]float64, nodePrice float64, hours float64) []float64 {
+	wastedResources := make([]float64, len(nodeResources))
+	//For each resource
+	for i, _ := range nodeResources {
+		totalUseOfResource := 0.0
+		//For each container
+		for j, _ := range usagePerContainer {
+			totalUseOfResource += usagePerContainer[j][i]
+		}
+		wastedResources[i] = nodeResources[i] - totalUseOfResource
+	}
+
+	//Calculate base cost and apply
+	return []float64{1}
+}
+
+func normalizeSlice(arr []float64) []float64 {
+	var sum float64 = 0
+	for _, n := range arr {
+		sum += n
+	}
+	toReturn := make([]float64, len(arr))
+	if sum == 0 {
+		for i, _ := range arr {
+			toReturn[i] = 1.0 / float64(len(arr))
+		}
+		return toReturn
+	}
+	for i, n := range arr {
+		toReturn[i] = n / sum
+	}
+	return toReturn
 }
--- models/models_test.go
+++ models/models_test.go
@@ -1,12 +1,38 @@
 package models
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+const epsilon float64 = 0.001
+
 func TestImportantFunction(t *testing.T) {
 	//assert = assert.New(t)
-	assert.Equal(t, 3, 3)
+	assert.InDelta(t, 3, 3, epsilon)
+}
+
+func TestNormalizeSlice(t *testing.T) {
+	slice := []float64{1}
+	slice = normalizeSlice(slice)
+	assert.InDelta(t, 1, slice[0], epsilon)
+
+	slice = []float64{2, 2}
+	slice = normalizeSlice(slice)
+	assert.InDelta(t, 0.5, slice[0], epsilon)
+
+	slice = []float64{9, 1}
+	slice = normalizeSlice(slice)
+	assert.InDelta(t, 0.1, slice[1], epsilon)
+	assert.InDelta(t, 0.9, slice[0], epsilon)
+
+	slice = []float64{rand.Float64(), rand.Float64(), rand.Float64(), rand.Float64(), rand.Float64()}
+	slice = normalizeSlice(slice)
+	sum := 0.0
+	for _, n := range slice {
+		sum += n
+	}
+	assert.InDelta(t, 1, sum, epsilon)
 }
