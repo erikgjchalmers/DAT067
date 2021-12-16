f7052467bcf61af1aab1cb3bc4a6d0777d12fdd9
erikgj <erikgj@student.chalmers.se>
2021-12-16 09:30:38 +0100

    Removed old printout code

--- main.go
+++ main.go
@@ -19,48 +19,7 @@ func main() {
 	 */
 	address := "http://localhost:9090"
 	//query := "up"
-
 	prometheus.CreateAPI(address)
-	memCapacity, warnings, err := prometheus.GetMemoryNodeCapacity("aks-standard1-15038067-vmss000001")
-	memUsage, warnings, err := prometheus.GetMemoryNodeUsage("aks-standard1-15038067-vmss000001")
-	cpuCapacity, warnings, err := prometheus.GetCPUNodeCapacity("aks-standard1-15038067-vmss000001")
-	cpuUsage, warnings, err := prometheus.GetCPUNodeUsage("aks-standard1-15038067-vmss000001")
-	costmodel := models.GoodModel{Balance: []float64{2, 1}}
-	price, _ := costmodel.CalculateCost([]float64{memCapacity, cpuCapacity}, [][]float64{{memUsage, cpuUsage}}, 10, 1)
-	fmt.Printf("Your node costs %f dollars.\n", price[0])
-
-	fmt.Printf("Data from Prometheus:\n\n")
-
-	// print all pods belonging to a specific node
-	resultPods := []string{}
-	resultPods, warnings, err = prometheus.GetPodsOfNode("aks-standard1-15038067-vmss000001")
-	fmt.Print("aks-standard1-15038067-vmss000001 hosting pods:\n\n")
-	fmt.Println(resultPods)
-	fmt.Print("\n\n")
-	resultPods, warnings, err = prometheus.GetPodsOfNode("aks-default-15038067-vmss000000")
-	fmt.Print("aks-default-15038067-vmss000000 hosting pods:\n\n")
-	fmt.Println(resultPods)
-	fmt.Print("\n\n")
-	resultPods, warnings, err = prometheus.GetPodsOfNode("aks-standard1-15038067-vmss000000")
-	fmt.Print("aks-standard1-15038067-vmss000000 hosting pods:\n\n")
-	fmt.Println(resultPods)
-	fmt.Print("\n\n")
-
-	if err != nil {
-		fmt.Printf("An error occured when querying Prometheus: %v\n", err)
-		os.Exit(1)
-	}
-
-	if len(warnings) > 0 {
-		fmt.Printf("Warnings during query: %v\n", warnings)
-	}
-
-	//print all pods and which deployment it belongs to
-	resultMap := make(map[string]string)
-	resultMap = prometheus.GetPodsToDeployment()
-	fmt.Print("print all pods and which deployment it belongs to:\n\n")
-	fmt.Print(resultMap)
-	fmt.Print(len(resultMap))
 
 	/* 	switch result.Type() {
 	   	case model.ValVector:
@@ -104,18 +63,6 @@ func main() {
 		return
 	}
 
-	azure.PrintNodes(pricedNodes)
-	resourceUsages, warnings, err := prometheus.GetPodsResourceUsage("aks-standard1-15038067-vmss000001")
-
-	if err != nil {
-		fmt.Printf("An error occured while retrieving pod resource usage from Prometheus: %s", err)
-		return
-	}
-
-	for key, value := range resourceUsages {
-		fmt.Printf("The pod '%s' is currently using %.6f CPU cores and %.1f bytes of RAM.\n", key, value.CpuUsage, value.MemUsage)
-	}
-
 	//Get nodes.
 	//Get nodes cost.
 	//pricedNodes, err := azure.GetPricedAzureNodes(clientSet)
@@ -144,7 +91,7 @@ func main() {
 			monster,
 			node.Price, 1)
 		index = 0
-		for pod, _ := range pods {
+		for pod := range pods {
 			podPrices[pod] = price[index]
 			index += 1
 		}
